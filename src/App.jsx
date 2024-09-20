0c14fa76309d3b5a5aebcc8f5d38e7209573b058
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-09-20 18:07:21 -0400

    Deletions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-// import Card from "./Card";
-// import Inventory from "./Inventory";
-// import CardInventory from "./CardInventory";
 import CardAdd from "./CardAdd";
-// import kegPic from "./assets/kegsouth.jpg";
+
 import FetchKeg from "./FetchKeg";
 import Footer from "./Footer";
 import { useState } from "react";
